afd0548b8d9a8b838926b7c543ba44ef728d5db3
Meghana korimi <meghnakorimi@gmail.com>
2024-06-03 15:58:55 +0530

    added navbar2

--- FinChamp/src/App.jsx
+++ FinChamp/src/App.jsx
@@ -5,15 +5,20 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import EntryPage from './Pages/EntryPage'
 import LoginPage from './Pages/LoginPage'
 import SignUpPage from './Pages/SignUpPage'
+import NavBar from './components/NavBar'
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/" element={<EntryPage />} />
-      </Routes>
-    </Router>
+    // <Router>
+    //   <Routes>
+    //     <Route path="/login" element={<LoginPage />} />
+    //     <Route path="/signup" element={<SignUpPage />} />
+    //     <Route path="/" element={<EntryPage />} />
+        
+    //   </Routes>
+    // </Router>
+    <div  className="container">
+      <NavBar/>
+    </div> 
   );
 }
 
--- FinChamp/src/components/NavBar.jsx
+++ FinChamp/src/components/NavBar.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
+import'../compoStyles/NavBar.css'
+import logo_light from '../assets/logo.png'
+
+import search_icon_light from '../assets/w.png'
 
 const NavBar = () => {
   return (
-    <div>
+    <div className="navbar">
+      <img src={logo_light} className="logo"/>
       <ul>
         <li>Home</li>
-        <li>News&Articles</li>
-        <li>Home</li>
-        <li>Home</li>
+        <li>Learning</li>
+        <li>Games</li>
+        <li>About</li>
+        <li><div className='search-box'>
+        <input type="text" placeholder='search' />
+        <img src={search_icon_light} alt=""/>
+      </div></li>
       </ul>
+      
+     
     </div>
   )
 }
